dbcc8d10269d0c9b33ca54d8c83ac886b51e6c20
Mario Gallardo Molino <mariete.b.fit@gmail.com>
2025-03-06 11:05:12 +0100

    Aztualizacion de menuprincipal

--- app/src/main/java/com/example/otakunikki/MenuPrincipal.java
+++ app/src/main/java/com/example/otakunikki/MenuPrincipal.java
@@ -1,18 +1,14 @@
 package com.example.otakunikki;
 
 import android.content.Intent;
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.PopupMenu;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -24,6 +20,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,24 +30,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MenuPrincipal extends AppCompatActivity {
-    private RecyclerView listViewHorizontal;
-    private AdaptadorLVHorAnimeRecomendaciones adpatdorAnimeRecomendado;
+    private ImageView imgFotoPrincipal;
+
+    private RecyclerView lvhAnimeRecomendaciones;
+    private AdaptadorLVHorAnimeMenuPrincipal adpatdorAnimeRecomendado;
     private List<Anime> listaAnimesRecomendados;
-    private RequestQueue requestQueue;
-    private StringRequest miStringRequest;
+    private RequestQueue rqAnimesRecomendados;
+    private StringRequest mrqAnimesRecomendados;
     private String urlRecomendados = "https://api.jikan.moe/v4/recommendations/anime";
     private BottomNavigationView menu_navegador;
+
+    private RecyclerView lvhAnimesTemporada;
+    private AdaptadorLVHorAnimeMenuPrincipal adaptadorAnimeTemporada;
+    private List<Anime> listaAnimeTemporada;
+    private RequestQueue rqAnimesTemporada;
+    private StringRequest mrqAnimesTemporada;
+    private String urlAnimeTemporada = "https://api.jikan.moe/v4/seasons/now";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_principal);
 
-        listViewHorizontal = findViewById(R.id.lvRecomendaciones);
-        listViewHorizontal.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        imgFotoPrincipal = findViewById(R.id.imgFotoPrincipal);
+        /**ADAPTADOR Y LISTVIEW PARA RECOMENDACIONES**/
+        lvhAnimeRecomendaciones = findViewById(R.id.lvRecomendaciones);
+        lvhAnimeRecomendaciones.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         listaAnimesRecomendados = new ArrayList<Anime>();
-        adpatdorAnimeRecomendado = new AdaptadorLVHorAnimeRecomendaciones(getApplicationContext(), listaAnimesRecomendados);
-        listViewHorizontal.setAdapter(adpatdorAnimeRecomendado);
+        adpatdorAnimeRecomendado = new AdaptadorLVHorAnimeMenuPrincipal(getApplicationContext(), listaAnimesRecomendados);
+        lvhAnimeRecomendaciones.setAdapter(adpatdorAnimeRecomendado);
         menu_navegador = findViewById(R.id.menu_navegador);
+
+        /**ADAPTADOR Y LISTVIEW PARA TEMPORADA**/
+        lvhAnimesTemporada = findViewById(R.id.lvAnimesTemporada);
+        lvhAnimesTemporada.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        listaAnimeTemporada = new ArrayList<Anime>();
+        adaptadorAnimeTemporada = new AdaptadorLVHorAnimeMenuPrincipal(getApplicationContext(), listaAnimeTemporada);
+        lvhAnimesTemporada.setAdapter(adaptadorAnimeTemporada);
 
         menu_navegador.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -69,31 +86,70 @@ public class MenuPrincipal extends AppCompatActivity {
         });
 
         CargarAnimesRecomendados();
+        CargarAnimesTemporada();
 
 
     }
 
-    public void abrirCuenta(){
-        Intent intent = new Intent(getApplicationContext(), info_usuario.class);
-        startActivity(intent);
+    private void CargarAnimesTemporada() {
+        rqAnimesTemporada = Volley.newRequestQueue(getApplicationContext());
+
+        mrqAnimesTemporada = new StringRequest(Request.Method.GET, urlAnimeTemporada, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    Log.i("INFO", response); // Ver la respuesta JSON
+
+                    JSONObject jsonResponse = new JSONObject(response);
+                    JSONArray dataArray = jsonResponse.getJSONArray("data");
+
+                    if (dataArray.length() == 0) {
+                        Toast.makeText(getApplicationContext(), "No hay datos disponibles", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    for (int i = 0; i < dataArray.length(); i++) {
+                        if(listaAnimeTemporada.size() == 10){
+                            int nroAleatorio = (int) (Math.random() * 10);
+                            Picasso.get().load(listaAnimeTemporada.get(nroAleatorio).getImagenPequenia()).into(imgFotoPrincipal);
+                            break;
+                        }
+
+                        JSONObject animeObject = dataArray.getJSONObject(i);
+                        int id = animeObject.getInt("mal_id");
+                        String titulo = animeObject.getString("title");
+                        String imagenGrande = animeObject.getJSONObject("images").getJSONObject("jpg").getString("large_image_url");
+                        String imagenPeqenia =animeObject.getJSONObject("images").getJSONObject("jpg").getString("small_image_url");
+                        Anime anime = new Anime(id, titulo, "", 0, imagenGrande, "", imagenPeqenia, null);
+
+                        if (!listaAnimeTemporada.contains(anime)) {
+                            listaAnimeTemporada.add(anime);
+                        }
+                    }
+
+                    // Notificar al adaptador después de agregar los datos
+                    adaptadorAnimeTemporada.notifyDataSetChanged();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Error procesando el JSON de temporada", Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("ERROR", "Error al obtener animes de temporada: " + error.getMessage());
+            }
+        });
+
+        rqAnimesTemporada.add(mrqAnimesTemporada);
     }
-    public void abrirForo(){
-        Intent intent = new Intent(getApplicationContext(), info_usuario.class);
-        startActivity(intent);
-    }
-    public void abrirExplorar(){
-        Intent intent = new Intent(getApplicationContext(), info_usuario.class);
-        startActivity(intent);
-    }
-    public void abrirListas(){
-        Intent intent = new Intent(getApplicationContext(), info_usuario.class);
-        startActivity(intent);
-    }
+
 
     private void CargarAnimesRecomendados() {
-        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        rqAnimesRecomendados = Volley.newRequestQueue(getApplicationContext());
 
-        miStringRequest = new StringRequest(Request.Method.GET, urlRecomendados, new Response.Listener<String>() {
+        mrqAnimesRecomendados = new StringRequest(Request.Method.GET, urlRecomendados, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 try {
@@ -133,8 +189,9 @@ public class MenuPrincipal extends AppCompatActivity {
 
                             }else{
                                 listaAnimesRecomendados.add(anime);
-                                adpatdorAnimeRecomendado.notifyDataSetChanged();
+
                                 if(listaAnimesRecomendados.size() == 10){
+                                    adpatdorAnimeRecomendado.notifyDataSetChanged();
                                     break;
                                 }
                             }
@@ -155,6 +212,24 @@ public class MenuPrincipal extends AppCompatActivity {
         });
 
         // Añadimos la solicitud a la cola de solicitudes
-        requestQueue.add(miStringRequest);
+        rqAnimesRecomendados.add(mrqAnimesRecomendados);
+    }
+
+    //METODOS PARA NAVEGAR ENTRE ACTIVIDADES
+    public void abrirCuenta(){
+        Intent intent = new Intent(getApplicationContext(), info_usuario.class);
+        startActivity(intent);
+    }
+    public void abrirForo(){
+        //Intent intent = new Intent(getApplicationContext(), info_usuario.class);
+        //startActivity(intent);
+    }
+    public void abrirExplorar(){
+        //Intent intent = new Intent(getApplicationContext(), info_usuario.class);
+        //startActivity(intent);
+    }
+    public void abrirListas(){
+        //Intent intent = new Intent(getApplicationContext(), info_usuario.class);
+        //startActivity(intent);
     }
 }
