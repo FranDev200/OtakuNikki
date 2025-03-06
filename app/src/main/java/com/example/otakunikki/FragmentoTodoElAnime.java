17d260bfbd1ed69cdd31cba00b025201e47e2fd6
AlmaCC <almacar2002@gmail.com>
2025-03-06 10:37:25 +0100

    Realizo cambios en explorar, añado el tab, y imagen

--- app/src/main/java/com/example/otakunikki/FragmentoTodoElAnime.java
+++ app/src/main/java/com/example/otakunikki/FragmentoTodoElAnime.java
@@ -10,8 +10,6 @@ import android.view.ViewGroup;
 
 public class FragmentoTodoElAnime extends Fragment {
 
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
