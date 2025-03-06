f2b50ac3ffdfbe9c40dc88543f871aa1bfc19382
AlmaCC <almacar2002@gmail.com>
2025-03-06 19:42:21 +0100

    ARREGLADO: el FragmentoTodoElAnime y el xml de explorar además añado el adaptador de GV

--- app/src/main/java/com/example/otakunikki/Explorar.java
+++ app/src/main/java/com/example/otakunikki/Explorar.java
@@ -1,6 +1,10 @@
 package com.example.otakunikki;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,26 +18,36 @@ import com.google.android.material.tabs.TabLayout;
 
 public class Explorar extends AppCompatActivity {
 
+    private ImageButton imgRetroceso;
     private TabLayout tabLayout;
     private ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("Explorar", "onCreate llamado");
+
         setContentView(R.layout.activity_explorar);
 
-
+        imgRetroceso = findViewById(R.id.imgRetroceso);
         viewPager2 = findViewById(R.id.vpPaginador);
         tabLayout = findViewById(R.id.tabLayout);
 
+        imgRetroceso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         viewPager2.setAdapter(new AdaptadorFragment(getSupportFragmentManager(), getLifecycle()));
-        viewPager2.setOrientation(viewPager2.ORIENTATION_HORIZONTAL);
         viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
 
             //Ahora, además para que la marca de tab seleccionado (linea horizontal) funcione al cambiar de Fragment activo
             //Es importante hacerlo porque si no, no irá sincronizado con el tabLayout.addOnTabSelectedListener() desarrollado debajo
             @Override
             public void onPageSelected(int position) {
+                Log.d("ViewPager2", "Página seleccionada: " + position);
                 tabLayout.selectTab(tabLayout.getTabAt(position));
             }
         });
@@ -41,6 +55,7 @@ public class Explorar extends AppCompatActivity {
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
+                Log.d("TabLayout", "Tab seleccionada en posición: " + tab.getPosition());
                 viewPager2.setCurrentItem(tab.getPosition());
             }
 
@@ -57,6 +72,10 @@ public class Explorar extends AppCompatActivity {
 
     }
 
+    public void abrirMenuPrincipal(){
+        Intent intent = new Intent(getApplicationContext(), FragmentInicio.class);
+        startActivity(intent);
+    }
     class AdaptadorFragment extends FragmentStateAdapter {
         public AdaptadorFragment(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
             super(fragmentManager, lifecycle);
@@ -65,13 +84,14 @@ public class Explorar extends AppCompatActivity {
         @NonNull
         @Override
         public Fragment createFragment(int position) {
+            Log.d("Viewpager2", "Creando gragmento en la pos: " + position);
             switch (position) {
                 case 0:
                     return new FragmentoTodoElAnime();
                 case 1:
                     return new FragmentoGeneros();
                 default:
-                    return new FragmentoUltTendencias();
+                    return  new FragmentoUltTendencias();
             }
         }
 
